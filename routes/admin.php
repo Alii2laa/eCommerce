9b37526ec2527bc03b4e4ab16836d6e9f907e5b3
Alii2laa <alialaaofficial@gmail.com>
2021-11-01 00:02:58 +0200

    End of Begin branch

---
+++ routes/admin.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Admin Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+// There is a prefix 'admin' in route service provider file
+Route::group(['namespace' => 'Dashboard','middleware'=>'auth:admin'],function (){
+
+    Route::get('/', 'dashboardController@index')->name('admin.dashboard');
+//    Route::group(['prefix'=>'settings'],function (){
+//        Route::get('shipping/{type}','SettingsController@editShippingMethod')->name('edit.shipping.method');
+//        Route::put('shipping/{id}','SettingsController@updateShippingMethod')->name('update.shipping.method');
+//    });
+
+});
+
+Route::group(['namespace' => 'Dashboard','middleware'=>'guest:admin'],function (){
+
+    Route::get('login', 'LoginController@loginView')->name('admin.login');
+    Route::post('login', 'LoginController@loginAdmin')->name('admin.post.login');
+
+
+});
